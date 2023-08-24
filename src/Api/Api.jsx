21879f8d045a2e0d8e89ea44d54bda7a20c414dd
sahad <msahadpp@gmail.com>
2023-08-24 15:15:55 +0530

    updated new baseUrl

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,3 +1,4 @@
-const baseUrl = 'https://api.medicoonline.online/';
+// const baseUrl = 'https://api.medicoonline.online/';
 // const baseUrl = 'http://localhost:8080'
+const baseUrl = 'https://medico-backend.onrender.com';
 export default baseUrl;
